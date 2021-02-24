03110a9de8a44c79f6ef96fa226dc7af27a1fc8a
AJ-Romera <ajromeraalfonso@gmail.com>
2021-02-24 18:55:01 +0100

    Primer Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className='container'>
+            <header>
+                <h1>Gasto Semanal</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
